9bc8dc6ffa192fb3173155a8700c9bca6147705b
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-15 12:19:45 -0400

    iniciando deploy

--- Servicio-Commons/src/main/java/com/galileo/cu/commons/models/ModelosBalizas.java
+++ Servicio-Commons/src/main/java/com/galileo/cu/commons/models/ModelosBalizas.java
@@ -1,7 +1,6 @@
 package com.galileo.cu.commons.models;
 
 import javax.persistence.Column;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
