68416e6221918264dba9d2774b2d7df5b02cc306
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-20 23:37:40 -0400

    ...

---
+++ Servicio-Commons/src/main/java/com/galileo/cu/commons/models/dto/BodyDelGroupPermissions.java
@@ -0,0 +1,17 @@
+package com.galileo.cu.commons.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+
+@Value
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class BodyDelGroupPermissions {
+    private long userId;
+    private int groupId;
+}
