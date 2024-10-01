ccecfc58e4d58b3f867846a64d1843b3d43963ad
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-01 02:41:57 -0400

    Recompilando main

--- Servicio-Commons/src/main/java/com/galileo/cu/commons/ServicioCommonsApplication.java
+++ Servicio-Commons/src/main/java/com/galileo/cu/commons/ServicioCommonsApplication.java
@@ -15,7 +15,7 @@ public class ServicioCommonsApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("COMMONS V24-09-28 05:44");
+		System.out.println("COMMONS V24-10-01 02:40");
 	}
 
 }
