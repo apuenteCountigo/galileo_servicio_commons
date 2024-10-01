0b23f1494c09b8c9ea0d908e2e55fd3444e7335c
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-01 01:33:55 -0400

    3- Recompilando para versión de PRE sin modelos balizas

--- Servicio-Commons/src/main/java/com/galileo/cu/commons/ServicioCommonsApplication.java
+++ Servicio-Commons/src/main/java/com/galileo/cu/commons/ServicioCommonsApplication.java
@@ -15,7 +15,7 @@ public class ServicioCommonsApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("COMMONS V-24-10-01 01:14");
+		System.out.println("COMMONS V-24-10-01 01:33");
 	}
 
 }
